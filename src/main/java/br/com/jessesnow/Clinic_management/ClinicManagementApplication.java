3d3d7d6ea97ea25d203654b2a482607404fca731
Jesse-Snow <jesse.neves@outlook.com>
2022-05-05 08:52:28 -0300

    - Maven database bugfix - End point GET retrieving data from database

--- src/main/java/br/com/jessesnow/Clinic_management/ClinicManagementApplication.java
+++ src/main/java/br/com/jessesnow/Clinic_management/ClinicManagementApplication.java
@@ -2,8 +2,9 @@ package br.com.jessesnow.Clinic_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"br.com.jessesnow.controller"})
+@SpringBootApplication(scanBasePackages = {"br.com.jessesnow.Clinic_management.controller"})
 public class ClinicManagementApplication {
 
 	public static void main(String[] args) {

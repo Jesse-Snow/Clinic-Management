fc9b6c8127e051fa2dfb4e1b3a1f9e0e915e574f
Jesse-Snow <jesse.neves@outlook.com>
2022-06-03 17:50:11 -0300

    Manually added Http response using spring

--- src/main/java/br/com/jessesnow/Clinic_management/controller/PacienteController.java
+++ src/main/java/br/com/jessesnow/Clinic_management/controller/PacienteController.java
@@ -6,6 +6,7 @@ import br.com.jessesnow.Clinic_management.repository.PacienteRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +26,7 @@ public class PacienteController {
   private PacienteRepository pacienteRepository;
 
   @GetMapping
+  @ResponseStatus(code = HttpStatus.OK) 
   public List<PacienteModel> findPacientes(){
     return pacienteRepository.findAll();
   }

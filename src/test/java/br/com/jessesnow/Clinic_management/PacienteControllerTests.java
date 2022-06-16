313c53d5286fb29411ec5004611adc80f548db22
Jesse-Snow <jesse.neves@outlook.com>
2022-06-16 17:44:04 -0300

    Added Test for Post (Need to fix this Test to prevent creating rows in table)

--- src/test/java/br/com/jessesnow/Clinic_management/PacienteControllerTests.java
+++ src/test/java/br/com/jessesnow/Clinic_management/PacienteControllerTests.java
@@ -1,6 +1,7 @@
 package br.com.jessesnow.Clinic_management;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import br.com.jessesnow.Clinic_management.model.PacienteModel;
@@ -19,6 +20,9 @@ public class PacienteControllerTests {
 
   @Autowired
   MockMvc mockMvc;
+
+  @Autowired 
+  ObjectMapper ObjectMapper;
 
   @Test
   public void GET_ALL_Should_Return_OK_And_TypeJson() throws Exception{
@@ -34,32 +38,30 @@ public class PacienteControllerTests {
      .andExpect(MockMvcResultMatchers.jsonPath("nomeCompleto").value("Sam Roger"));
  }
 
-// @Test
-//public void POST_Should_Return_Ok_And_Verify_Response() throws Exception{
-//  PacienteModel pacienteModel = new PacienteModel();
-//  pacienteModel.setCpf("6134124123");                          
-//  pacienteModel.setSexo("F");                        
-//  pacienteModel.setEmail("samus@email");
-//  pacienteModel.setIdade(31);
-//  pacienteModel.setCelular("91341346");
-//  pacienteModel.setEndereco("Far from this Planet,Dark Aether");
-//  pacienteModel.setDiagnostico("Infecção por X Parasite");
-//  pacienteModel.setMedicamentos("Injeção Metroid");
-//  pacienteModel.setPlanoDeSaude(0);
-//  pacienteModel.setProcedimentos("Utilizar Injeção Metroid");
-//  pacienteModel.setNomePlanoDeSaude("");
-//
-//  this.mockMvc.perform(MockMvcRequestBuilders.post("/pacientes")
-//    .contentType(MediaType.APPLICATION_JSON)
-//    .content(asJsonString(pacienteModel))
-//    .andExpect(MockMvcResultMatchers.status().isCreated());
-//}
 
-   public static String asJsonString(final Object obj) {
-    try {
-      return new ObjectMapper().writeValueAsString(obj);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+  // Need to fix this and Delete this Row in Database
+  @Test 
+  public void POST_Should_Return_Ok() throws Exception {
+    PacienteModel pacienteModel = new PacienteModel();
+    pacienteModel.setnomeCompleto("Samus Aram");
+    pacienteModel.setCpf("6134124125553");                          
+    pacienteModel.setSexo("F");                        
+    pacienteModel.setEmail("samus@email");
+    pacienteModel.setIdade(31);
+    pacienteModel.setCelular("91341346");
+    pacienteModel.setEndereco("Far from this Planet,Dark Aether");
+    pacienteModel.setDiagnostico("Infecção por X Parasite");
+    pacienteModel.setMedicamentos("Injeção Metroid");
+    pacienteModel.setPlanoDeSaude(0);
+    pacienteModel.setProcedimentos("Utilizar Injeção Metroid");
+    pacienteModel.setNomePlanoDeSaude("");
+    String requestJson = ObjectMapper.writeValueAsString(pacienteModel); 
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post("/pacientes")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson)
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk());
     }
-  }
+
 }

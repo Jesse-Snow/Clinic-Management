a51511fc70970cff10b97be7e81f1e05e6cd6a31
Jesse-Snow <jesse.neves@outlook.com>
2022-07-22 15:51:04 -0300

    Removed unnecessary code

--- src/main/java/br/com/jessesnow/Clinic_management/repository/PacienteRepository.java
+++ src/main/java/br/com/jessesnow/Clinic_management/repository/PacienteRepository.java
@@ -12,5 +12,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PacienteRepository extends JpaRepository<PacienteModel,Integer>{
 
-  long deleteByNomeCompleto(String nomeCompleto);
 }
